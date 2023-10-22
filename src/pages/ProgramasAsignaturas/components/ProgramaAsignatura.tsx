14f94b81431968b2ed8f62dbc7d5e26540f808bb
orellanagonzalo64 <orellanagonzalo64@gmail.com>
2023-10-22 16:27:53 -0300

    Cambios en la seccion de descriptores

--- src/pages/ProgramasAsignaturas/components/ProgramaAsignatura.tsx
+++ src/pages/ProgramasAsignaturas/components/ProgramaAsignatura.tsx
@@ -1,10 +1,60 @@
 // import { useParams } from 'react-router-dom'
+import { useState } from 'react'
 import CargaHoraria from './CargaHoraria'
 import InformacionAdicional from './InformacionAdicional'
 import SeccionDescriptores from './SeccionDescriptores'
 
+type CargaHorariaType = {
+  cargaHorariaTotal: number
+  semanasDictado: number
+  teoriaPresencial: number
+  practicaPresencial: number
+  teoricoPracticoPresencial: number
+  laboratoriosPresenciales: number
+  teoriaDistancia: number
+  practicaDistancia: number
+  teoricoPracticoDistancia: number
+  laboratorioDistancia: number
+}
+
+type DescriptorType = {
+  resultadosAprendizaje: string[]
+  ejesTransversales: {
+    nombre: string
+    valor: number
+  }[]
+  descriptores: {
+    si: { id: number; nombre: string }[]
+    no: { id: number; nombre: string }[]
+  }
+  actividadesReservadas: string[]
+}
+
+
+
+
+type ProgramaAsignaturaType = {
+  cargaHoraria: CargaHorariaType
+}
+
 export default function ProgramaAsignatura() {
   //const { id } = useParams()
+  const [programaAsignatura, setProgramaAsignatura] =
+    useState<ProgramaAsignaturaType>({
+      cargaHoraria: {
+        cargaHorariaTotal: 0,
+        semanasDictado: 0,
+        teoriaPresencial: 0,
+        practicaPresencial: 0,
+        teoricoPracticoPresencial: 0,
+        laboratoriosPresenciales: 0,
+        teoriaDistancia: 0,
+        practicaDistancia: 0,
+        teoricoPracticoDistancia: 0,
+        laboratorioDistancia: 0
+      }
+    })
+
   return (
     <section className="section-content">
       <h1>ProgramaAsignatura</h1>
--- src/pages/ProgramasAsignaturas/components/SeccionDescriptores.tsx
+++ src/pages/ProgramasAsignaturas/components/SeccionDescriptores.tsx
@@ -3,11 +3,43 @@ import { camelCase } from 'lodash'
 import Button from '../../../components/ui/Button'
 import Modal from '../../../components/Modal/Modal'
 
+// Definir type
+type Descriptor = {
+  resultadosAprendizaje: string[]
+  ejesTransversales: {
+    nombre: string
+    valor: number
+  }[]
+  descriptores: {
+    si: { id: number; nombre: string }[]
+    no: { id: number; nombre: string }[]
+  }
+  actividadesReservadas: string[]
+}
+
 export default function SeccionDescriptores() {
-  const [isOpen, setIsOpen] = useState(false)
-  const [formValues, setFormValues] = useState({
-    resultadosAprendizaje: [],
-    ejesTransversales: [],
+  const [modalResultadosAbierto, setModalResultadosAbierto] = useState(false)
+  const [modalEjesTransversales, setModalEjesTransversales] = useState(false)
+  const [formValues, setFormValues] = useState<Descriptor>({
+    resultadosAprendizaje: [''],
+    ejesTransversales: [
+      {
+        nombre: 'Eje 1',
+        valor: 0
+      },
+
+      {
+        nombre: 'Eje 2',
+        valor: 2
+      }
+    ],
+    descriptores: {
+      si: [
+        { id: 1, nombre: 'Descriptor 1' },
+        { id: 2, nombre: 'Descriptor 2' }
+      ],
+      no: [{ id: 3, nombre: 'Descriptor 3' }]
+    },
     actividadesReservadas: []
   })
 
@@ -18,6 +50,85 @@ export default function SeccionDescriptores() {
       ...formValues,
       [camelCase(name)]: value
     })
+  }
+
+  const handleResultadosAprendizajeChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>,
+    index: number
+  ) => {
+    const { value } = e.target
+
+    const resultadosAprendizaje = [...formValues.resultadosAprendizaje]
+    resultadosAprendizaje[index] = value
+
+    setFormValues({
+      ...formValues,
+      resultadosAprendizaje
+    })
+  }
+
+  const resultadosAprendizajeCount = () => {
+    //Contar solo los resultados de aprendizaje que no sean string vacios
+    return formValues.resultadosAprendizaje.filter((item) => item !== '').length
+  }
+
+  const aniadirResultadoAprendizaje = () => {
+    setFormValues({
+      ...formValues,
+      resultadosAprendizaje: [...formValues.resultadosAprendizaje, '']
+    })
+  }
+
+  // Al abrir el modal limpiamos los resultados de aprendizaje que son string vacios
+  const abrirModalResultados = () => {
+    const resultadosAprendizaje = [...formValues.resultadosAprendizaje]
+    const resultadosAprendizajeLimpios = resultadosAprendizaje.filter(
+      (item) => item !== ''
+    )
+    if (resultadosAprendizajeLimpios.length === 0)
+      resultadosAprendizajeLimpios.push('')
+
+    setFormValues({
+      ...formValues,
+      resultadosAprendizaje: resultadosAprendizajeLimpios
+    })
+    setModalResultadosAbierto(true)
+  }
+
+  const handleEjeTransversalChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const { value } = e.target
+
+    const ejesTransversales = [...formValues.ejesTransversales]
+    ejesTransversales[index].valor = parseInt(value)
+
+    setFormValues({
+      ...formValues,
+      ejesTransversales
+    })
+  }
+
+
+  const handleDescriptorChange = (id: number, tipo: string) => {
+    const descriptores = { ...formValues.descriptores };
+    const sourceArray = tipo === 'si' ? descriptores.si : descriptores.no;
+    const targetArray = tipo === 'si' ? descriptores.no : descriptores.si;
+    
+    const descriptorIndex = sourceArray.findIndex(descriptor => descriptor.id === id);
+  
+    if (descriptorIndex !== -1) {
+      const descriptor = sourceArray[descriptorIndex];
+      // Removemos el descriptor del array de origen y lo agregamos al array de destino
+      sourceArray.splice(descriptorIndex, 1);
+      targetArray.push(descriptor);
+    }
+  
+    setFormValues({
+      ...formValues,
+      descriptores
+    });
   }
 
   return (
@@ -33,10 +144,10 @@ export default function SeccionDescriptores() {
               type="text"
               id="resultados-aprendizaje"
               name="resultados_aprendizaje"
-              value={formValues.resultadosAprendizaje.length}
+              value={resultadosAprendizajeCount()}
               onChange={handleChange}
             />
-            <Button text="+" onClick={() => setIsOpen(true)} />
+            <Button text="+" onClick={abrirModalResultados} />
           </div>
           <label htmlFor="ejes-transversales">Ejes transversales</label>
           <div className="modal-input">
@@ -47,7 +158,7 @@ export default function SeccionDescriptores() {
               value={formValues.ejesTransversales.length}
               onChange={handleChange}
             />
-            <Button text="+" />
+            <Button text="+" onClick={() => setModalEjesTransversales(true)} />
           </div>
           <label htmlFor="actividades-reservadas">Actividades reservadas</label>
           <div className="modal-input">
@@ -60,10 +171,98 @@ export default function SeccionDescriptores() {
             />
             <Button text="+" />
           </div>
+          <label htmlFor="descriptores">Descriptores</label>
+          <br />
+          <div className="selector-descritores">
+            <div className="selector-descriptores-columna">
+              <label>Si</label>
+
+              {formValues.descriptores.si.map((descriptor) => (
+                <p onClick={() => handleDescriptorChange(descriptor.id, 'si')}>
+                  {descriptor.nombre}
+                </p>
+              ))}
+            </div>
+            <div className="selector-descriptores-columna">
+              <label>No</label>
+              {formValues.descriptores.no.map((descriptor) => (
+                <p onClick={() => handleDescriptorChange(descriptor.id, 'no')}>
+                  {descriptor.nombre}
+                </p>
+              ))}
+            </div>
+          </div>
         </form>
       </section>
-      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-        MODAL
+      <Modal
+        open={modalResultadosAbierto}
+        modalTitle="Resultados de aprendizaje"
+        onClose={() => setModalResultadosAbierto(false)}
+      >
+        {formValues.resultadosAprendizaje.map((_, index) => (
+          <>
+            {/* {TODO: CREAR COMPONENTE} */}
+            <textarea
+              value={formValues.resultadosAprendizaje[index]}
+              onChange={(e) => handleResultadosAprendizajeChange(e, index)}
+              rows={4}
+              cols={50}
+            />
+            {index === formValues.resultadosAprendizaje.length - 1 && (
+              <div className="sumar-text-area">
+                <Button text="+" onClick={aniadirResultadoAprendizaje}></Button>
+              </div>
+            )}
+          </>
+        ))}
+      </Modal>
+      <Modal
+        open={modalEjesTransversales}
+        modalTitle="Ejes transversales"
+        onClose={() => setModalEjesTransversales(false)}
+      >
+        {formValues.ejesTransversales.map((eje, index) => (
+          <>
+            <label>{eje.nombre}</label>
+            {/* {TODO: CREAR COMPONENTE RADIO BUTTONS} */}
+
+            <div className="radio-buttons">
+              <input
+                type="radio"
+                name={eje.nombre}
+                value="0"
+                defaultChecked
+                checked={eje.valor === 0}
+                onChange={(e) => handleEjeTransversalChange(e, index)}
+              />
+              Nada
+              <input
+                type="radio"
+                name={eje.nombre}
+                value="1"
+                checked={eje.valor === 1}
+                onChange={(e) => handleEjeTransversalChange(e, index)}
+              />
+              Bajo
+              <input
+                type="radio"
+                name={eje.nombre}
+                value="2"
+                checked={eje.valor === 2}
+                onChange={(e) => handleEjeTransversalChange(e, index)}
+              />
+              Medio
+              <input
+                type="radio"
+                name={eje.nombre}
+                value="3"
+                checked={eje.valor === 3}
+                onChange={(e) => handleEjeTransversalChange(e, index)}
+              />
+              Alto
+            </div>
+          </>
+        ))}
       </Modal>
     </>
   )
