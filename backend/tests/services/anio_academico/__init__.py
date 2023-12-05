a407c0a0ead381faabec1558e5e448c0cbc1759a
Rosalba Caldez <rochu.caldez@gmail.com>
2023-12-05 17:52:30 -0300

    Test obtener anio siguiente

--- backend/tests/services/anio_academico/__init__.py
+++ backend/tests/services/anio_academico/__init__.py
@@ -1,4 +1,4 @@
-from .test_obtener_anio_actual import TestObtenerSemestreActual
-from .test_obtener_anio_siguiente import TestObtenerSemestreSiguiente
-from .test_obtener_anio_anterior import TestObtenerSemestreAnterior
+from .test_obtener_anio_actual import TestObtenerAnioAcademicoActual
+from .test_obtener_anio_siguiente import TestObtenerAnioAcademicoSiguiente
+from .test_obtener_anio_anterior import TestObtenerAnioAcademicoAnterior
 from .test_validar_anio import TestValidarAnioAcademico
---
+++ backend/tests/services/anio_academico/test_obtener_anio_siguiente.py
@@ -0,0 +1,92 @@
+from freezegun import freeze_time
+from datetime import time, datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+from backend.common.mensajes_de_error import MENSAJE_NO_HAY_ANIOS_FUTUROS
+from backend.services import ServicioAnioAcademico
+from backend.models import AnioAcademico
+from backend.tests.utils import (
+    crear_anios_de_prueba,
+    FECHA_INICIO_ANIO_CERRADO,
+    FECHA_FIN_ANIO_ABIERTO,
+    FECHA_FIN_ANIO_FUTURO,
+)
+
+
+class TestObtenerAnioAcademicoSiguiente(TestCase):
+    servicio_anio_academico = ServicioAnioAcademico()
+
+    def setUp(self) -> None:
+        (
+            self.anio_cerrado,
+            self.anio_actual,
+            self.anio_futuro,
+        ) = crear_anios_de_prueba()
+
+    def test_hay_dos_anios_futuros(self):
+        AnioAcademico.objects.create(
+            fecha_inicio=FECHA_FIN_ANIO_FUTURO + timezone.timedelta(days=1),
+            fecha_fin=FECHA_FIN_ANIO_FUTURO + timezone.timedelta(days=181),
+        )
+
+        anio = self.servicio_anio_academico.obtener_anio_academico_siguiente()
+        self.assertEqual(anio, self.anio_futuro)
+
+    def test_probar_antes_de_media_noche_en_cambio_de_anio(self):
+        hora_de_referencia = time(hour=23, minute=59, second=59)
+        fecha_de_referencia = timezone.make_aware(
+            datetime.combine(FECHA_FIN_ANIO_ABIERTO, hora_de_referencia)
+        )
+        with freeze_time(fecha_de_referencia):
+            anio = self.servicio_anio_academico.obtener_anio_academico_siguiente()
+
+        self.assertEqual(anio, self.anio_futuro)
+
+    def test_probar_despues_de_media_noche_en_cambio_de_anio(self):
+        dia_de_referencia = FECHA_INICIO_ANIO_CERRADO + timezone.timedelta(days=1)
+        hora_de_referencia = time(hour=0, minute=0, second=0)
+        fecha_de_referencia = timezone.make_aware(
+            datetime.combine(dia_de_referencia, hora_de_referencia)
+        )
+        with freeze_time(fecha_de_referencia):
+            anio = self.servicio_anio_academico.obtener_anio_academico_siguiente()
+        self.assertEqual(anio, self.anio_actual)
+
+    def test_no_se_crearon_anios(self):
+        AnioAcademico.objects.all().delete()
+
+        with self.assertRaises(ValidationError) as contexto:
+            self.servicio_anio_academico.obtener_anio_academico_siguiente()
+
+        self.assertIn("__all__", contexto.exception.message_dict)
+        self.assertIn(
+            MENSAJE_NO_HAY_ANIOS_FUTUROS,
+            contexto.exception.message_dict["__all__"],
+        )
+
+    def test_no_hay_anios_siguientes(self):
+        self.anio_futuro.delete()
+
+        with self.assertRaises(ValidationError) as contexto:
+            self.servicio_anio_academico.obtener_anio_academico_siguiente()
+
+        self.assertIn("__all__", contexto.exception.message_dict)
+        self.assertIn(
+            MENSAJE_NO_HAY_ANIOS_FUTUROS,
+            contexto.exception.message_dict["__all__"],
+        )
+
+    def test_no_hay_anio_actual(self):
+        fecha_referencia = FECHA_FIN_ANIO_FUTURO + timezone.timedelta(days=7)
+        with freeze_time(fecha_referencia):
+            with self.assertRaises(ValidationError) as contexto:
+                self.servicio_anio_academico.obtener_anio_academico_siguiente()
+
+            self.assertIn("__all__", contexto.exception.message_dict)
+            self.assertIn(
+                MENSAJE_NO_HAY_ANIOS_FUTUROS,
+                contexto.exception.message_dict["__all__"],
+            )
