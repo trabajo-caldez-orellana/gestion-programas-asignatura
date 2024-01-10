136467ff36734b8e3bc5b993f6ba2dd37edae642
Rosalba Caldez <rochu.caldez@gmail.com>
2024-01-09 21:58:31 -0300

    comment

--- backend/services/roles.py
+++ backend/services/roles.py
@@ -4,6 +4,5 @@ from backend.models import Usuario, Rol
 class ServicioRoles:
     def obtener_roles_usuario(self, usuario: Usuario):
         roles = Rol.objects.filter(usuario=usuario)
-        print(roles)
 
         return roles
