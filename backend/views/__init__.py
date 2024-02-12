6e9c71136aefce744fc8e36c3b8f4a3369f634cb
Rosalba Caldez <rochu.caldez@gmail.com>
2024-02-12 17:46:34 -0300

    Agregar llamado a la API para reutilizar

--- backend/views/__init__.py
+++ backend/views/__init__.py
@@ -4,5 +4,6 @@ from .programas_de_asignatura import (
     InformacionProgramaAPI,
     InformacionNuevoProgramaAPI,
     InformacionModificacionProgramaAPI,
+    ReutilizarUltimoPrograma
 )
 from .historial.filtros_historial import ObtenerFiltrosHistorial
