ab9eceea825b314913509b9c0e7a6bc667df8a15
Rosalba Caldez <rochu.caldez@gmail.com>
2024-03-11 20:23:02 -0300

    Todo para setear un proyect

--- backend/common/funciones_fecha.py
+++ backend/common/funciones_fecha.py
@@ -7,3 +7,20 @@ def obtener_fecha_y_hora_actual() -> timezone.datetime:
 
 def obtener_fecha_actual() -> timezone.datetime.date:
     return obtener_fecha_y_hora_actual().date()
+
+def crear_fecha_y_hora(
+    year: int, month: int, day: int, hour: int = 0, minute: int = 0, second: int = 0
+):
+    return timezone.make_aware(
+        timezone.datetime(
+            year=year, month=month, day=day, hour=hour, minute=minute, second=second
+        )
+    )
+
+def crear_fecha(
+    year: int, month: int, day: int
+):
+    return timezone.datetime(
+            year=year, month=month, day=day
+        ).date()
+
