82822f170f7038c6c74b0f941b800db375c9faa9
Rosalba Caldez <rochu.caldez@gmail.com>
2024-03-14 21:23:07 -0300

    Componentes para reutilizar con styled components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,15 @@ import Modal from './Modal/Modal'
 import Sidebar from './Sidebar/Sidebar'
 import Table from './Table/Table'
 import ProtectedRoute from './ProtectedRoute'
+import { Titulo, Subtitulo, TituloSeccion } from './ui/Titulo'
 
-export { Navbar, Modal, Sidebar, Table, ProtectedRoute }
+export {
+  Navbar,
+  Modal,
+  Sidebar,
+  Table,
+  ProtectedRoute,
+  Titulo,
+  Subtitulo,
+  TituloSeccion
+}
---
+++ src/components/ui/Titulo.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Titulo = styled.h1`
+  color: var(--dark-color);
+  font-size: 30px;
+  width: 100%;
+  text-align: center;
+  margin: 30px auto;
+  box-sizing: border-box;
+`
+
+export const Subtitulo = styled.h2`
+  color: black;
+  text-align: center;
+  margin: 0;
+`
+
+export const TituloSeccion = styled.h3`
+  color: var(--dark-color);
+  font-size: 20px;
+  text-align: left;
+`
