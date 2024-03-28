34ec62edc6d9642138a184313562aba7e3b197db
Rosalba Caldez <rochu.caldez@gmail.com>
2024-03-28 12:33:19 -0300

    Vars

--- src/utils/axiosClient.tsx
+++ src/utils/axiosClient.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios'
-
-const BASE_URL = 'http://127.0.0.1:8000'
+import { BASE_URL } from '../helpers/env-variables'
 export const TOKEN_KEY = 'token'
 
 export const client = axios.create({
