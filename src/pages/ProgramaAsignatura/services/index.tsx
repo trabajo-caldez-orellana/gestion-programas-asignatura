ea1d8cfa5a492b9619909e6b50f0a6b84e086eaf
Rosalba Caldez <rochu.caldez@gmail.com>
2024-03-29 17:36:10 -0300

    modificar interfaz error

--- src/pages/ProgramaAsignatura/services/index.tsx
+++ src/pages/ProgramaAsignatura/services/index.tsx
@@ -308,6 +308,8 @@ const parseProgramaPOSTError = (
         ? responseError.extension_docentes[0]
         : ''
     },
+    // TODO. Cambiar cuadno cree la llamada a a la api
+    correlativas: '',
     all: responseError.__all__ ? responseError.__all__[0] : '',
     mensaje: responseError.mensaje ? responseError.mensaje[0] : ''
   }
