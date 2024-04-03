d99462ccc3230f6a6e1965b6a8e5eb7e3a98b4d1
Rosalba Caldez <rochu.caldez@gmail.com>
2024-04-03 11:29:58 -0300

    Fix logout

--- backend/settings.py
+++ backend/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     "rest_framework",
     'rest_framework_simplejwt',
     "rest_framework.authtoken",
+    'rest_framework_simplejwt.token_blacklist',
     "social_django",
     "backend",
 ]
