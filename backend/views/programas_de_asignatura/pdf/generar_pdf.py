969ed82e9ac7c8d1a71d24f14db867edded67a9f
Rosalba Caldez <rochu.caldez@gmail.com>
2024-04-05 15:05:40 -0300

    Agregar footer y header al pdf

--- backend/views/programas_de_asignatura/pdf/generar_pdf.py
+++ backend/views/programas_de_asignatura/pdf/generar_pdf.py
@@ -1,4 +1,4 @@
-from weasyprint import HTML
+from weasyprint import HTML, CSS
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
@@ -9,7 +9,12 @@ from django.template.loader import get_template
 
 from backend.services import ObtenerDatosPdf
 
+def get_page_body(boxes):
+    for box in boxes:
+        if box.element_tag == 'body':
+            return box
 
+        return get_page_body(box.all_children())
 class GenerarPDF(APIView):
     permission_classes = [
         IsAuthenticated,
@@ -32,15 +37,43 @@ class GenerarPDF(APIView):
             "bloque_curricular": datos_programa["bloque_curricular"],
         }
 
-        template = get_template("programa_de_asignatura.html")
+        # Main template
+        main_html = get_template('programa_de_asignatura.html')
+        main_doc = main_html.render(context)
+        main_doc = HTML(string=main_doc).render()
+        print(main_doc.pages)
 
-        # Renderiza el template con el contexto
-        html_content = template.render(context)
+        # Template of header
+        header_html = get_template('documento_header.html')
+        header = header_html.render()
+        header = HTML(string=header)
+        header = header.render(stylesheets=[CSS(string='@page {size:A4; margin:0 1cm;} body {position: fixed; top: 0cm;}')])
 
-        # Convierte el contenido HTML en un PDF utilizando WeasyPrint
-        pdf_file = HTML(string=html_content).write_pdf()
+        header_page = header.pages[0]
+        header_body = get_page_body(header_page._page_box.all_children())
+        header_body = header_body.copy_with_children(header_body.all_children())
 
+        # Template of footer
+        footer_html = get_template('documento_footer.html')
+        footer = footer_html.render()
+        footer = HTML(string=footer)
+        footer = footer.render(stylesheets=[CSS(string='@page {size:A4; margin:0 1cm;} body {position: fixed; bottom: 0;}')])
+
+        footer_page = footer.pages[0]
+        footer_body = get_page_body(footer_page._page_box.all_children())
+        footer_body = footer_body.copy_with_children(footer_body.all_children())
+
+
+        # Insert header and footer in main doc
+        for _, page in enumerate(main_doc.pages):
+
+            page_body = get_page_body(page._page_box.all_children())
+
+            page_body.children += header_body.all_children()
+            page_body.children += footer_body.all_children()
+
+        pdf = main_doc.write_pdf()
         # Devuelve el PDF como una respuesta HTTP
-        response = HttpResponse(pdf_file, content_type="application/pdf")
+        response = HttpResponse(pdf, content_type="application/pdf")
         response["Content-Disposition"] = 'inline; filename="programa.pdf"'
         return response
