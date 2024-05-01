934a7d46f3c293c9b0ba566ed1aa248cf062b7af
Rosalba Caldez <rochu.caldez@gmail.com>
2024-04-30 21:13:35 -0300

    celery corre

--- backend/celery.py
+++ backend/celery.py
@@ -3,9 +3,15 @@ from celery import Celery
 
 from django.conf import settings
 
+from backend.tasks import enviar_email_async
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 app = Celery("backend", broker='redis://redis:6379')
+app.conf.timezone = settings.TIME_ZONE
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
-app.conf.timezone = settings.TIME_ZONE
+app.register_task(enviar_email_async)
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
